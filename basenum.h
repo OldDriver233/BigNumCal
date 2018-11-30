e2e1cdaec30a3a3a045c44e9a1721ca112f66a67
OldDriver233 <1147972163@qq.com>
2018-11-30 21:27:02 +0800

    Base2

--- basenum.h
+++ basenum.h
@@ -5,5 +5,22 @@ class bnum{
         std::vector<int> beforeDot,afterDot;
         bool withDot;
     public:
-
+    bnum();
+    bnum(std::string s);
 };
+
+bnum atoi(std::string str)
+{
+    bnum a;
+    return a;
+}
+
+bnum::bnum()
+{
+    withDot=false;
+}
+
+bnum::bnum(std::string s)
+{
+
+}
