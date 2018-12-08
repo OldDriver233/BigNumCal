b4cf40b03c1acd7b4a0d335d7b305f728f046de0
OldDriver233 <1147972163@qq.com>
2018-12-08 17:27:02 +0800

    UpdConv

--- converter.h
+++ converter.h
@@ -4,10 +4,24 @@ using namespace std;
 
 bnum stob(std::string s)
 {
-
+    bnum rt;
+    rt.beforeDot.resize(s.length()+3);
+    for(int i=s.length()-1;i>=0;i--)
+    {
+        rt.beforeDot[i]=s[i]-'0';
+    }
+    return rt;
 }
 
 bnum itob(long long a)
 {
-    
+    bnum rt;
+    int cnt=0;
+    while(a!=0)
+    {
+        rt.beforeDot[cnt]=a%10;
+        a/=10;
+        cnt++;
+    }
+    return rt;
 }
