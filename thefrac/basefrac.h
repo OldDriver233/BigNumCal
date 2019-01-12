16eac8a6fece2fc42503d9262cf9358a73dcb069
Sun2 <1147972163@qq.com>
2019-01-12 11:12:45 +0800

    Frac update

--- thefrac/basefrac.h
+++ thefrac/basefrac.h
@@ -5,5 +5,26 @@ class frac
     private:
         bnum upper,lower;
     public:
-        
+        frac();
+        frac(std::string s);
+        frac(double d);
+
+        bool operator==(frac a);
+        bool operator<(frac a);
+        bool operator>(frac a);
+
+        frac operator+(frac a);
+        frac operator-(frac a);
+        frac operator*(frac a);
+        frac operator/(frac a);
+        frac operator%(frac a);
+
+        void operator+=(frac a);
+        void operator-=(frac a);
+        void operator*=(frac a);
+        void operator/=(frac a);
+        void operator%=(frac a);
+
+        void operator=(frac a);
 };
+
